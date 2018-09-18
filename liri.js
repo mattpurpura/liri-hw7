71c2f1430bd67024fa0a731451b75d7c618d3f3d
mattpurpura <mpurpura212@gmail.com>
2018-09-17 21:47:24 -0700

    Liri app is working

--- liri.js
+++ liri.js
@@ -2,34 +2,109 @@ var dotenv = require("dotenv").config();
 var request = require("request");
 var moment = require("moment");
 var Spotify = require('node-spotify-api') 
+var inquirer = require("inquirer")
 var fs = require("fs");
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
-console.log(spotify);
 
-var command = process.argv[2];
-var searchTerm = process.argv.splice(3).join(" ");
+var pastSearches = [];
 
+fs.readFile("random.txt", "utf8", function(error, data){
+    if(error){
+        console.log(error);
+    }
+    pastSearches = data.split(",");
+})
+
+function runLiri(){
+inquirer.prompt([
+    {
+        type: "list",
+        message: "How can I help you?",
+        choices: ["Find a song", "Find a movie", "Find a concert", "View Past Searches"],
+        name: "greeting"
+    },
+        { 
+            when: function(response){
+                return response.greeting === "Find a movie";
+            },
+            type: "input", 
+            message: "What movie are you looking for?",
+            name: "movieTitle" 
+        },
+        { 
+            when: function(response){
+                return response.greeting === "Find a concert";
+            },
+            type: "input", 
+            message: "What artist would you like to see live?",
+            name: "artist" 
+        },
+        { 
+            when: function(response){
+                return response.greeting === "Find a song";
+            },
+            type: "input", 
+            message: "What song would you like to hear?",
+            name: "song" 
+        },
+        { 
+            when: function(response){
+                return response.greeting === "View Past Searches";
+            },
+            type: "list", 
+            message: "Here is your search history:",
+            choices: function(){
+                let searchHistory = [];
+                for(let i=1; i<pastSearches.length; i+=2){
+                    searchHistory.push(pastSearches[i]);
+                }
+                return searchHistory;
+            },
+            name: "past" 
+        },
+])
+.then(function(response){
+var command = response.greeting
+var searchTerm;
+var surpriseTerm;
 askLiri();
-
 function askLiri(){
 switch(command){
-    case "concert-this":
+    case "Find a concert":
+    if (response.greeting === "View Past Searches"){
+        
+    }
+    else if(response.artist === undefined){
+        searchTerm = dataArr[1];
+    }
+    else{
+        searchTerm = response.artist;
+    }
     function concertCommand(){
 
         request("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp", function(error, response, body){
             let concertInfo = JSON.parse(body);
             let venue = concertInfo[0].venue.name;
             let location = concertInfo[0].venue.city+", "+concertInfo[0].venue.country;
-            let date = concertInfo[0].datetime;
+            let date = moment(concertInfo[0].datetime).format("MM/DD/YYYY");
             console.log(venue, location, date);
         })
     }
     concertCommand();
     break;
-    case "spotify-this-song":
+    case "Find a song":
+    if(response.greeting === "View Past Searches"){
+
+    }
+    else if(response.song === undefined){
+        searchTerm = surpriseTerm;
+    }
+    else{
+       searchTerm = response.song; 
+    }
         function songCommand(){
             // console.log(songTitle);
 
@@ -40,8 +115,14 @@ switch(command){
                 let songArtist = songData.artists[0].name;
                 // console.log(songArtist);
                 let songAlbum = songData.album.name;
-                let songLink = songData.href;
-                console.log(songLink);
+                let songLink = songData.preview_url;
+                if(songData.preview_url === null){
+                    console.log("There is no preview link for this song")
+                }
+                else{
+                    console.log(songLink);
+                }
+                
                 console.log(songName, songArtist, songAlbum);
                 if (err) {
                     return console.log('Error occurred: ' + err);
@@ -50,13 +131,18 @@ switch(command){
         }   
         songCommand();
     break;
-    case "movie-this":
+    case "Find a movie":
+    if(response.greeting === "View Past Searches"){
+
+    }
+    else if(response.movieTitle === undefined){
+        searchTerm = "Mr. Nobody"
+    }
+    else{
+        searchTerm = response.movieTitle;
+    }
+
     function movieCommand(){
-
-        if (process.argv[3]===undefined){
-            searchTerm = "Mr.+Nobody";
-        }
-
 
         request("http://www.omdbapi.com/?t="+searchTerm+"&y=&plot=short&apikey=trilogy", function(error, response, body){
                 let movieInfo = JSON.parse(body);
@@ -74,10 +160,8 @@ switch(command){
             })
         }
         movieCommand();
-        +
-        6./8*9/3
     break;
-    case "do-what-it-says":
+    case "Surprise me":
         fs.readFile("random.txt", "utf8", function(error, data){
             if (error){
                 console.log(error)
@@ -85,9 +169,46 @@ switch(command){
             let dataArr = data.split(",")
             console.log(dataArr);
             command = dataArr[0];
-            searchTerm = dataArr[1];
+            surpriseTerm = dataArr[1];
             askLiri();
         })
     break;
-}
-}
+    case "View Past Searches":
+        searchTerm = response.past;
+        command = pastSearches[pastSearches.indexOf(searchTerm)-1];
+        askLiri();
+    break;
+} // ends switch case
+var searchQuery = command + "," + searchTerm;
+
+if (pastSearches.includes(searchTerm) === false){
+   fs.appendFile("random.txt", ","+searchQuery, function(error){
+    if(error){
+        console.log(error)
+    }
+})//ends appendFile 
+} // ends if pastSearches includes statement
+} // ends askLiri function
+
+setTimeout(anythingElse, 2000)
+function anythingElse(){
+    inquirer.prompt([
+        {
+            type: "confirm", 
+            message: "Is there anything else I can help you with?",
+            name:"continue"
+        }
+    ])
+    .then(function(response){
+        if(response.continue === true){
+            runLiri();
+        }
+        else{
+            console.log("Goodbye.")
+        }
+    }) 
+}// ends anythingElse
+
+}) //ends then after first inquirer prompt
+} // ends runLiri
+runLiri();
